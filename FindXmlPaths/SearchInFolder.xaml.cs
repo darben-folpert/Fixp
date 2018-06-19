d4641fcbb965416a1cf3f9b6baf85bbac4c0729b
davoo <david.lagier@gmail.com>
2018-06-19 02:39:57 +0200

    Update / quick test of Process button on 'search in folder' interface

--- FindXmlPaths/SearchInFolder.xaml.cs
+++ FindXmlPaths/SearchInFolder.xaml.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
 
 namespace FindXmlPathsWpf
 {
@@ -22,6 +22,12 @@ namespace FindXmlPathsWpf
         public SearchInFolder()
         {
             InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Get wrecked !!",
+                    "Error: Owned !", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
     }
 }
